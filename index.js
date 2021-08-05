7d3df8a05db2afc4cae7f21a344a964bd6e7ff3b
Sadman Amin <sadmanamin@Sadmans-MacBook-Pro.local>
2021-08-05 15:06:13 -0700

    a commit

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+/*
 app.all("/*", function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
 });
+*/
 
 
 app.post('/sms', (req, res) => {
