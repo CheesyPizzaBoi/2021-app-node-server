a141ef63c855ec265c91a72924d187f1c4aab999
Sadman Amin <sadmanamin@Sadmans-MacBook-Pro.local>
2021-08-05 15:13:05 -0700

    a commit

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.all("/*", function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
