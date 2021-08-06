8daa7c9a9c2d81f654d603bcc5c23b94742af6f3
Sadman Amin <sadmanamin@Sadmans-MacBook-Pro.local>
2021-08-05 21:12:35 -0700

    a commit

--- index.js
+++ index.js
@@ -83,14 +83,14 @@ const { stringify } = require('@angular/compiler/src/util');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-/*
+
 app.all("/*", function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
   });
-*/
+
 
 app.get("/", (req, res) => {
     
